0b552eae3eb5ef6adac9236a03a22f3e41e0e789
Matt Barraza-Carl <mccarl@asu.edu>
2017-08-25 12:43:32 -0700

    add tsv

--- first_scraping.py
+++ first_scraping.py
@@ -27,16 +27,39 @@
 # # for list in soup.find_all('li'):
 # # 	print(list.text)
 # ###########
+# , encoding='utf8'
+
+# import csv; 
+# import requests; 
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+
+# with open('tweets_local_results.csv', 'w', newline='') as outfile:
+#     writer = csv.writer(outfile)
+#     writer.writerow(['date','time','tweets'])
+
+#     # sauce = requests.get('https://www.washingtonpost.com/graphics/politics/100-days-of-trump-tweets/?utm_term=.0c2052f6d858',headers={"User-Agent":"Existed"}).text
+#     sauce = urlopen("file:///Users/booboo/Rithm/scrape_the_tweets/index_tweets_wapo.html").read()
+#     soup = BeautifulSoup(sauce,"html.parser")
+
+#     for item in soup.select("li.pg-excerpt.visible"):
+#         date = item.get('data-date')
+#         time = item.select("span.gray")[0].text
+#         title = item.text.strip()
+#         print(date, time, title[10:])
+#         writer.writerow([date, time, title[10:]])
 
 import csv; 
 import requests; 
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-with open('tweets_results.csv', 'w', newline='', encoding='utf8') as outfile:
+with open('tweets_local_results.tsv', 'w', newline='') as outfile:
     writer = csv.writer(outfile)
     writer.writerow(['date','time','tweets'])
 
-    sauce = requests.get('https://www.washingtonpost.com/graphics/politics/100-days-of-trump-tweets/?utm_term=.0c2052f6d858',headers={"User-Agent":"Existed"}).text
+    # sauce = requests.get('https://www.washingtonpost.com/graphics/politics/100-days-of-trump-tweets/?utm_term=.0c2052f6d858',headers={"User-Agent":"Existed"}).text
+    sauce = urlopen("file:///Users/booboo/Rithm/scrape_the_tweets/data/index_tweets_wapo.html").read()
     soup = BeautifulSoup(sauce,"html.parser")
 
     for item in soup.select("li.pg-excerpt.visible"):
