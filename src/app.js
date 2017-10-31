ec29e3d523fffd05b71ed3eb01aed97bded04852
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-10-31 08:11:22 -0700

    Double quotes to single quotes

--- src/app.js
+++ src/app.js
@@ -1,63 +1,70 @@
-//import { getRandom } from '/utls/generateRandNumber.js'; works with chrome 61
+import { getRandom } from './utls/generateRandNumber'; //works with chrome 61
 
-const svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g").attr("transform", "translate(18," + (height / 4) + ")");
-    
+const svg = d3.select('svg'),
+  width = +svg.attr('width'),
+  height = +svg.attr('height'),
+  g = svg.append('g').attr('transform', 'translate(18,' + height / 4 + ')');
+
 d3.csv('data/tweets_local_results.csv', function(error, twitter) {
-    // returns array of tweets
-    console.log(twitter)
-    const dumbTweets = twitter.map(tweet => tweet.tweets)
-    const needsABarOfSoap = dumbTweets[getRandom(0, dumbTweets.length-1)]
-    // returns keyed object
-    const copy = twitter.map((d) => {
-        return {
-            date: d.date,
-            time: d.time,
-            tweet: d.tweets
-        }
-    })
+  // returns array of tweets
+  console.log(twitter);
+  const dumbTweets = twitter.map(tweet => tweet.tweets);
+  const needsABarOfSoap = dumbTweets[getRandom(0, dumbTweets.length - 1)];
+  // returns keyed object
+  const copy = twitter.map(d => {
+    return {
+      date: d.date,
+      time: d.time,
+      tweet: d.tweets
+    };
+  });
 
-function update(data) {
-  const t = d3.transition()
-      .duration(3000);
+  function update(data) {
+    const t = d3.transition().duration(5000);
 
-  let text = g.selectAll("text")
-    .data(data, function(d) { return d; });
-  text.exit()
-       .attr("class", "exit")
-    .transition(t)
-       .attr("y", 100)
-       .style("fill-opacity", 1e-6)
-       .remove();
-  text.attr("class", "update")
-       .attr("y", 0)
-       .style("fill-opacity", 1)
-    .transition(t)
-       .attr("x", function(d, i) { return i * 10});
-  text.enter().append("text")
-       .attr("class", "enter")
-       .attr("dy", "10em")
-       .attr("y", -60)
-       .attr("x", function(d, i) { return i * 10; })
-       .style("fill-opacity", 1e-6)
-       .text(function(d) { return d; })
-    .transition(t)
-       .attr("y", 0)
-       .style("fill-opacity", 1); 
-}
+    let text = g.selectAll('text').data(data, function(d) {
+      return d;
+    });
+    text
+      .exit()
+      .attr('class', 'exit')
+      .transition(t)
+      .attr('y', 100)
+      .style('fill-opacity', 1e-6)
+      .remove();
+    text
+      .attr('class', 'update')
+      .attr('y', 0)
+      .style('fill-opacity', 1)
+      .transition(t)
+      .attr('x', function(d, i) {
+        return i * 10;
+      });
+    text
+      .enter()
+      .append('text')
+      .attr('class', 'enter')
+      .attr('dy', '10em')
+      .attr('y', -60)
+      .attr('x', function(d, i) {
+        return i * 10;
+      })
+      .style('fill-opacity', 1e-6)
+      .text(function(d) {
+        return d;
+      })
+      .transition(t)
+      .attr('y', 0)
+      .style('fill-opacity', 1);
+  }
 
+  update(dumbTweets[getRandom(0, dumbTweets.length - 1)]);
 
-update(dumbTweets[getRandom(0, dumbTweets.length-1)]);
-
-d3.interval(function() {
+  d3.interval(function() {
     update(dumbTweets[getRandom(0, dumbTweets.length - 1)]);
-}, 3500);
+  }, 7500);
+});
 
-})
-
-function getRandom(floor, ceiling) {
-    return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
-}
-
+// function getRandom(floor, ceiling) {
+//   return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
+// }
